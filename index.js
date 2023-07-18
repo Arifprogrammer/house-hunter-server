d2f9e2c3b6063c8c4f7e2aa3382d64e08c2ac80d
Arifprogrammer <arifmgtcu@gmail.com>
2023-07-18 18:11:57 +0600

    integrated with mongodb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const corsOptions = {
   origin: "*",
@@ -14,6 +15,36 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+//* ingrating with mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ketp048.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("houseHunter").collection("users");
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//* testing the server
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
