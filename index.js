6a4dcfbf42ae1133e26ccab85acbcf0ee96a96dd
Arifprogrammer <arifmgtcu@gmail.com>
2023-07-18 17:49:34 +0600

    first commit with initial setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+//* middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
